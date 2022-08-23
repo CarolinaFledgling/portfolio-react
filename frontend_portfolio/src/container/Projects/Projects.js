fd3545fc663231f60be9b6144f3e7a74b139009d
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-08-23 13:04:12 +0200

    started work on markup of projects section

--- frontend_portfolio/src/container/Projects/Projects.js
+++ frontend_portfolio/src/container/Projects/Projects.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Projects.scss'
 
 const Projects = () => {
+    const [activeFilter, setActiveFilter] = useState('All');
+    const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+    const handleProjectFilter = () => {
+
+    }
     return (
-        <div>Projects</div>
+        <>
+            <h2 className="head-text">There's nothing quite like building <span className="gradient__text"> projects</span>  to grow coding skills.</h2>
+            <div className='app_work-filter'>
+                {["React", "Wordpress", "HTML/CS/Js", "Picapoint", "All"].map((item, index) => {
+                    <div
+                        key={`project-${index}`}
+                        onClick={() => handleProjectFilter(item)}
+                        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''} `}
+                    >
+
+                    </div>
+                })}
+            </div>
+
+            <motion.div
+                animate={animateCard}
+                transition={{ duration: 0.5, delayChildren: 0.5 }}
+                className="app__work-portfolio"
+            >
+
+            </motion.div>
+        </>
     )
 }
 
