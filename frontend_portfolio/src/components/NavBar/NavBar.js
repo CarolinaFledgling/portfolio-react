547b3a54167a51f53e350a4231ada8a3f18d5e00
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-08-24 09:22:56 +0200

    working on RWD in navbar and header section

--- frontend_portfolio/src/components/NavBar/NavBar.js
+++ frontend_portfolio/src/components/NavBar/NavBar.js
@@ -9,39 +9,41 @@ import './NavBar.scss'
 const NavBar = () => {
     const [toggleNav, setToggleNav] = useState(false)
     return (
-        <nav className='app__navbar navbar__section-padding'>
-            <div className='app__navbar-logo'>
-                <span className="gradient__text">Karolina.K</span>
-            </div>
-            <ul className='app__navbar-links'>
-                {['header', 'skills', 'projects', 'contact'].map((item) => (
-                    <li className='app_flex p-text' key={`link-${item}`}>
-                        <div>
-                        </div>
-                        <a href={`#${item}`}>{item}</a>
-                    </li>
-                ))}
-            </ul>
 
-            <div className='app__navbar-menu'>
-                <HiMenuAlt4 onClick={() => setToggleNav(true)} />
+        <nav className='app__navbar'>
+            <div className='app__navbar-container container'>
+                <span className="app__navbar-logo">Karolina.K </span>
+                <ul className='app__navbar-links'>
+                    {['header', 'skills', 'projects', 'contact'].map((item) => (
+                        <li className='app_flex p-text' key={`link-${item}`}>
+                            <div>
+                            </div>
+                            <a href={`#${item}`}>{item}</a>
+                        </li>
+                    ))}
+                </ul>
 
-                {toggleNav && (
-                    <motion.div whileInView={{ x: [300, 0] }}
-                        transition={{ duration: 0.85, ease: 'easeOut' }}>
-                        <HiX onClick={() => setToggleNav(false)} />
-                        <ul>
-                            {['header', 'skills', 'projects', 'contact'].map((item) => (
-                                <li key={item}>
-                                    <a href={`#${item}`} onClick={() => setToggleNav(false)} >{item}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </motion.div>
-                )
-                }
+                <div className='app__navbar-menu'>
+                    <HiMenuAlt4 onClick={() => setToggleNav(true)} />
+
+                    {toggleNav && (
+                        <motion.div whileInView={{ x: [300, 0] }}
+                            transition={{ duration: 0.85, ease: 'easeOut' }}>
+                            <HiX onClick={() => setToggleNav(false)} />
+                            <ul>
+                                {['header', 'skills', 'projects', 'contact'].map((item) => (
+                                    <li key={item}>
+                                        <a href={`#${item}`} onClick={() => setToggleNav(false)} >{item}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </motion.div>
+                    )
+                    }
+                </div>
             </div>
         </nav >
+
     )
 }
 
--- frontend_portfolio/src/container/Header/Header.js
+++ frontend_portfolio/src/container/Header/Header.js
@@ -6,18 +6,18 @@ import { AppWrap } from '../../wrapper';
 
 const Header = () => {
   return (
-    <div id="header">
+    <div id="header" className='section'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
-        className='app__header section__padding'
+        className='app__header container'
       >
         <div className="app__header-content">
-          <h1 className="text">Hi there! <span>👋</span></h1>
-          <p>I&apos;m a self-taught <span className="gradient__text span-text">Frontend Developer</span> with 1 year commercial experience using <span className='app__header-bold'>React</span>, <span className='app__header-bold'>Sanity (CMS)</span>, <span className='app__header-bold'>JS</span>, <span className='app__header-bold'>HTML</span>, <span className='app__header-bold'>CSS</span>, <span className='app__header-bold'>Next.js</span>, <span className='app__header-bold'>Formik</span>, <span className='app__header-bold'>MUI</span>.</p>
+          <h1>Hi there! <span>👋</span></h1>
+          <p>I&apos;m a self-taught <span className="gradient__text">Frontend Developer</span> with 1 year commercial experience using <span className='app__header-bold'>React</span>, <span className='app__header-bold'>Sanity (CMS)</span>, <span className='app__header-bold'>JS</span>, <span className='app__header-bold'>HTML</span>, <span className='app__header-bold'>CSS</span>, <span className='app__header-bold'>Next.js</span>, <span className='app__header-bold'>Formik</span>, <span className='app__header-bold'>MUI</span>.</p>
           <p>I&apos;m interested in continuing building both Static and SPA web applications for both desktop and mobile devices.</p>
-          <p>I&apos;m  now spending my time building projects with React JS, Sanity, and
-            learning new technologies.</p>
+          <p>Now I&apos;m spending my time building projects with React JS, Sanity, and
+            learning new technologies.</p> <span className="span-text"> I'm open for new opportunities.</span>
         </div>
 
         <div className="app__header-image">
@@ -28,4 +28,4 @@ const Header = () => {
   )
 }
 
-export default AppWrap(Header)
+export default Header
