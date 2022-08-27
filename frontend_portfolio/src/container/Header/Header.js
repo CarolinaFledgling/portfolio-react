283ce0dcb1e4c554a81bcb6675c3fdec7da37889
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-08-27 12:25:05 +0200

    work ontext in header section

--- frontend_portfolio/src/container/Header/Header.js
+++ frontend_portfolio/src/container/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.scss'
 
 const Header = () => {
   return (
-    <div id="about" className='section'>
+    <header id="about" className='section'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -13,17 +13,19 @@ const Header = () => {
       >
         <div className="app__header-content">
           <h1>Hi there! <span>👋</span></h1>
-          <p>I&apos;m a self-taught <span className="gradient__text">Frontend Developer</span> with 1 year commercial experience using: <span className='app__header-bold'>React</span>, <span className='app__header-bold'>Sanity (CMS)</span>, <span className='app__header-bold'>JS</span>, <span className='app__header-bold'>HTML</span>, <span className='app__header-bold'>CSS</span>, <span className='app__header-bold'>Next.js</span>, <span className='app__header-bold'>Formik</span>, <span className='app__header-bold'>MUI</span>.</p>
+          <p>I&apos;m a self-taught <span className="gradient__text">Frontend Developer</span> with 1 year commercial experience using: <span className='app__header-bold'>React</span>, <span className='app__header-bold'>Sanity (CMS)</span>, <span className='app__header-bold'>JavaScript</span>, <span className='app__header-bold'>Html</span>, <span className='app__header-bold'>Css</span>, <span className='app__header-bold'>Next.js</span>, <span className='app__header-bold'>Formik</span>, <span className='app__header-bold'> and working with Rest API</span>.</p>
           <p>I&apos;m interested in continuing building both Static and SPA web applications for both desktop and mobile devices.</p>
-          <p>Now I&apos;m spending my time building projects with React JS, Sanity, and
-            learning new technologies.</p> <span className="span-text"> I'm open for new opportunities.</span>
+          <p>Now I&apos;m spending my time building projects with React, Sanity, and
+            learning new technologies.</p>
+          <p>In my spare time I love playing team sports:⚽🏀🏐🎾🏓🏸and Nintendo games 🎮 </p>
+          <span className="span-text"> I'm open for new opportunities.</span>
         </div>
 
         <div className="app__header-image">
           <img src={header} alt="illustration of a dancing girl" />
         </div>
       </motion.div>
-    </div>
+    </header>
   )
 }
 
