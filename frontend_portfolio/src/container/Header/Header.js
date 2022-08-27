4e845d32e43c21b584fff0272d4be9b0ad48d071
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-08-27 22:07:08 +0200

    displaying rich text from Sanity,  trying to add custom styles the span in Portable Text to get the same styles when we use gradient_text class. - need to be fix

--- frontend_portfolio/src/container/Header/Header.js
+++ frontend_portfolio/src/container/Header/Header.js
@@ -5,6 +5,20 @@ import './Header.scss'
 import { PortableText } from '@portabletext/react'
 import { getImageBuilder, client } from '../../client';
 
+
+
+const myPortableTextComponents = {
+  marks: {
+    text: ({ children, value }) => {
+      return (
+        <span className="gradient__text">
+          {children}
+        </span>
+      )
+    },
+  },
+}
+
 const Header = () => {
   const [dataBlockContent, setDataBlockContent] = useState([])
 
@@ -31,19 +45,29 @@ const Header = () => {
         transition={{ duration: 0.5 }}
         className='app__header container'
       >
-        {dataBlockContent.map((dataBlock) => {
+        {dataBlockContent.map((dataBlock, index) => {
           console.log('datablock', { dataBlock })
           const url = getUrl(dataBlock)
           return (
-            <div className="app__header-container">
+            <div key={`datablock-${index}`} className="app__header-container">
               <div className="app__header-content">
                 <h1>{dataBlock.title}</h1>
-                <p>I&apos;m a self-taught <span className="gradient__text">Frontend Developer</span> with 1 year commercial experience using: <span className='app__header-bold'>React</span>, <span className='app__header-bold'>Sanity (CMS)</span>, <span className='app__header-bold'>JavaScript</span>, <span className='app__header-bold'>Html</span>, <span className='app__header-bold'>Css</span>, <span className='app__header-bold'>Next.js</span>, <span className='app__header-bold'>Formik</span>, <span className='app__header-bold'> and working with Rest API</span>.</p>
+                {/* <p>I&apos;m a self-taught <span className="gradient__text">Frontend Developer</span> with 1 year commercial experience using: <span className='app__header-bold'>React</span>, <span className='app__header-bold'>Sanity (CMS)</span>, <span className='app__header-bold'>JavaScript</span>, <span className='app__header-bold'>Html</span>, <span className='app__header-bold'>Css</span>, <span className='app__header-bold'>Next.js</span>, <span className='app__header-bold'>Formik</span>, <span className='app__header-bold'> and working with Rest API</span>.</p>
                 <p>I&apos;m interested in continuing building both Static and SPA web applications for both desktop and mobile devices.</p>
                 <p>Now I&apos;m spending my time building projects with React, Sanity, and
                   learning new technologies.</p>
                 <p>In my spare time I love playing team sports:⚽🏀🏐🎾🏓🏸and Nintendo games 🎮 </p>
-                <span className="span-text"> I'm open for new opportunities.</span>
+                <span className="span-text"> I'm open for new opportunities.</span> */}
+                <PortableText
+                  value={dataBlock.textBlock}
+
+                  serializers={{
+                    span: (props) => <span className="gradient__text" {...props} />,
+                    someCustomType: Header,
+
+                  }}
+                />
+
               </div>
 
               <div className="app__header-image">
