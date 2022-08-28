6dcbdaa2eb4a02fd44ace6f6f854b89e51e4c694
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-08-28 13:42:00 +0200

    clean comments

--- frontend_portfolio/src/container/Header/Header.js
+++ frontend_portfolio/src/container/Header/Header.js
@@ -54,21 +54,9 @@ const Header = () => {
             <div key={`datablock-${index}`} className="app__header-container">
               <div className="app__header-content">
                 <h1>{dataBlock.title}</h1>
-                {/* <p>I&apos;m a self-taught <span className="gradient__text">Frontend Developer</span> with 1 year commercial experience using: <span className='app__header-bold'>React</span>, <span className='app__header-bold'>Sanity (CMS)</span>, <span className='app__header-bold'>JavaScript</span>, <span className='app__header-bold'>Html</span>, <span className='app__header-bold'>Css</span>, <span className='app__header-bold'>Next.js</span>, <span className='app__header-bold'>Formik</span>, <span className='app__header-bold'> and working with Rest API</span>.</p>
-                <p>I&apos;m interested in continuing building both Static and SPA web applications for both desktop and mobile devices.</p>
-                <p>Now I&apos;m spending my time building projects with React, Sanity, and
-                  learning new technologies.</p>
-                <p>In my spare time I love playing team sports:⚽🏀🏐🎾🏓🏸and Nintendo games 🎮 </p>
-                <span className="span-text"> I'm open for new opportunities.</span> */}
+    
                 <PortableText
                   value={dataBlock.textBlock}
-
-                  // serializers={{
-                  //   span: (props) => <span className="gradient__text" {...props} />,
-
-
-                  // }}
-
                   components={myPortableTextComponents}
                 />
 
