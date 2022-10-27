2439a74fb06d1c3ef27293ee819393386a19dbac
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-10-27 21:46:09 +0200

    added height and width to the images

--- frontend_portfolio/src/components/MeditingImg/MeditingImg.js
+++ frontend_portfolio/src/components/MeditingImg/MeditingImg.js
@@ -4,7 +4,7 @@ import "./MeditingImg.scss";
 function MeditingImg() {
   return (
     <div className="app__contact-img">
-      <img src={meditating} alt="meditating img" />
+      <img src={meditating} alt="meditating img"  height="300px" weight="300px"/>
     </div>
   );
 }
--- frontend_portfolio/src/container/Projects/Projects.js
+++ frontend_portfolio/src/container/Projects/Projects.js
@@ -6,171 +6,173 @@ import "./Projects.scss";
 import getUrlFromProject from "../../helpers/getUrlFromProject";
 
 const Projects = () => {
-    const [activeFilter, setActiveFilter] = useState("All");
-    const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-    const [projects, setProjects] = useState([]);
-    const [filterProjects, setFilterProjects] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [projects, setProjects] = useState([]);
+  const [filterProjects, setFilterProjects] = useState([]);
 
-    useEffect(() => {
-        const query = '*[_type== "projects"]';
+  useEffect(() => {
+    const query = '*[_type== "projects"]';
 
-        client
-            .fetch(query)
-            .then((data) => {
-                setProjects(data);
-                setFilterProjects(data);
-                console.log("[data from projects]", data);
-            })
-            .catch((err) => {
-                console.log("Sanity error in projects", err);
-            });
-    }, []);
+    client
+      .fetch(query)
+      .then((data) => {
+        setProjects(data);
+        setFilterProjects(data);
+        console.log("[data from projects]", data);
+      })
+      .catch((err) => {
+        console.log("Sanity error in projects", err);
+      });
+  }, []);
 
-    const handleProjectFilter = (item) => {
-        setActiveFilter(item);
-        setAnimateCard([{ y: 100, opacity: 0 }]);
+  const handleProjectFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
-        setTimeout(() => {
-            setAnimateCard([{ y: 0, opacity: 1 }]);
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
 
-            if (item === "All") {
-                setFilterProjects(projects);
-            } else {
-                const filteredProjectItem = projects.filter((project) =>
-                    project.tags.includes(item)
-                );
-                setFilterProjects(filteredProjectItem);
-            }
-        }, 500);
-    };
+      if (item === "All") {
+        setFilterProjects(projects);
+      } else {
+        const filteredProjectItem = projects.filter((project) =>
+          project.tags.includes(item)
+        );
+        setFilterProjects(filteredProjectItem);
+      }
+    }, 500);
+  };
 
-    return (
-        <section id="projects" className="app__projects section">
-            <div className="container">
-                <h2 className="app__projects-heading-text">
-                    There's nothing quite like building{" "}
-                    <span className="color-text"> projects</span> to grow coding skills.
-                </h2>
-                <p className="app__projects-description">Filter projects by: commercial, personal and all </p>
-                <div className="app__project-filter">
-
-                    {[
-                        "Commercial project",
-                        "Personal project",
-                        "All",
-                    ].map((item, index) => {
-                        return (
-                            <div
-                                key={`project-${index}`}
-                                onClick={() => handleProjectFilter(item)}
-                                className={`app__project-filter-item app__flex p-text ${activeFilter === item ? "item-active" : ""
-                                    } `}
-                            >
-                                {item}
-                            </div>
-                        );
-                    })}
-                </div>
-
-                <motion.div
-                    animate={animateCard}
-                    transition={{ duration: 0.5, delayChildren: 0.5 }}
-                    className="app__project-portfolio"
+  return (
+    <section id="projects" className="app__projects section">
+      <div className="container">
+        <h2 className="app__projects-heading-text">
+          There's nothing quite like building{" "}
+          <span className="color-text"> projects</span> to grow coding skills.
+        </h2>
+        <p className="app__projects-description">
+          Filter projects by: commercial, personal and all{" "}
+        </p>
+        <div className="app__project-filter">
+          {["Commercial project", "Personal project", "All"].map(
+            (item, index) => {
+              return (
+                <div
+                  key={`project-${index}`}
+                  onClick={() => handleProjectFilter(item)}
+                  className={`app__project-filter-item app__flex p-text ${
+                    activeFilter === item ? "item-active" : ""
+                  } `}
                 >
-                    {filterProjects?.map((project, index) => {
-                        const url = getUrlFromProject(project);
-                        return (
-                            //card
-                            <div className="app__project-item" key={`project-${index}`}>
-                                {/* img */}
+                  {item}
+                </div>
+              );
+            }
+          )}
+        </div>
 
-                                {url ? (
-                                    <img src={getUrlFromProject(project)} alt={project.title} />
-                                ) : (
-                                    <span>No Image</span>
-                                )}
+        <motion.div
+          animate={animateCard}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__project-portfolio"
+        >
+          {filterProjects?.map((project, index) => {
+            const url = getUrlFromProject(project);
+            return (
+              //card
+              <div className="app__project-item" key={`project-${index}`}>
 
-                                {/* card body */}
-                                <div className="app__project-content">
-                                    <a
-                                        href={project.projectLink}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        aria-label={`${ project.title} link`}
-                                    >
-                                        <h3 className="app__projects-title">{project.title}</h3>
-                                    </a>
-                                    <p className="app__project-description">
-                                        {project.description}
-                                    </p>
+                {url ? (
+        
+                    <img src={getUrlFromProject(project)} alt={project.title} />
+            
+                ) : (
+                  <span>No Image</span>
+                )}
 
-                                    <div className="app__project-tag">
-                                        <p className="app__project-paragraph">{project.tags[0]}</p>
-                                    </div>
-                                    <div className="app__project-icons">
-                                        {project.projectLink ? (
-                                            <a
-                                                href={project.projectLink}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                                aria-label={`${project.title} link`}
-                                            >
-                                                <div className="app__project-icon">
-                                                    <AiFillEye />
-                                                </div>
-                                            </a>
-                                        ) : (
-                                            <p className="app__project-noLink">Link Coming Soon</p>
-                                        )}
-
-                                        {project.codeLink && (
-                                            <a
-                                                href={project.codeLink}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                                aria-label={`${project.title} link`}
-                                            >
-                                                <div className="app__project-icon">
-                                                    <AiFillGithub />
-                                                </div>
-                                            </a>
-                                        )}
-                                    </div>
-                                    <div className="app__project-technologies">
-                                        <div className="app__technologies-header">
-                                            <p>Technologies:</p>
-                                        </div>
-                                        <div className="app__technologies-items">
-                                            {project.technologies?.map((item, index) => {
-                                                return (
-                                                    <span key={index}>
-                                                        {item}
-                                                        {index === project.technologies?.length - 1 ? "" : ","}
-                                                    </span>
-                                                );
-                                            })}
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </motion.div>
-            </div>
-            <div className="app-project-button">
-                <a
-                    className="primary-button"
+                {/* card body */}
+                <div className="app__project-content">
+                  <a
+                    href={project.projectLink}
                     target="_blank"
                     rel="noreferrer"
-                    href="https://github.com/CarolinaFledgling"
-                    aria-label="link to github projects"
-                >
-                    ALL GITHUB REPOS
-                </a>
-            </div>
-        </section>
-    );
+                    aria-label={`${project.title} link`}
+                  >
+                    <h3 className="app__projects-title">{project.title}</h3>
+                  </a>
+                  <p className="app__project-description">
+                    {project.description}
+                  </p>
+
+                  <div className="app__project-tag">
+                    <p className="app__project-paragraph">{project.tags[0]}</p>
+                  </div>
+                  <div className="app__project-icons">
+                    {project.projectLink ? (
+                      <a
+                        href={project.projectLink}
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label={`${project.title} link`}
+                      >
+                        <div className="app__project-icon">
+                          <AiFillEye />
+                        </div>
+                      </a>
+                    ) : (
+                      <p className="app__project-noLink">Link Coming Soon</p>
+                    )}
+
+                    {project.codeLink && (
+                      <a
+                        href={project.codeLink}
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label={`${project.title} link`}
+                      >
+                        <div className="app__project-icon">
+                          <AiFillGithub />
+                        </div>
+                      </a>
+                    )}
+                  </div>
+                  <div className="app__project-technologies">
+                    <div className="app__technologies-header">
+                      <p>Technologies:</p>
+                    </div>
+                    <div className="app__technologies-items">
+                      {project.technologies?.map((item, index) => {
+                        return (
+                          <span key={index}>
+                            {item}
+                            {index === project.technologies?.length - 1
+                              ? ""
+                              : ","}
+                          </span>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+      <div className="app-project-button">
+        <a
+          className="primary-button"
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/CarolinaFledgling"
+          aria-label="link to github projects"
+        >
+          ALL GITHUB REPOS
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
